139811b1b16a21e59aa0200f7f1b9e1d60509959
Dark_ <Dark_@UNTARGETABLE-DE>
2018-02-23 17:11:05 +0700

    l5 gallery

--- public/js/s/5.js
+++ public/js/s/5.js
@@ -4,7 +4,7 @@ $(function () {
         url:'/ajax/5',
         dataType: "json",
         success: function (d, e) {
-           console.log(d);
+       console.log(d);
            $('#gallery').text(d.gallery);
            $.each(d.images, function (index, item) {
                $('#img-'+index+' img').attr('src',item.file).attr('alt',item.name).attr('width',400).attr('height',400);
@@ -13,5 +13,6 @@ $(function () {
         error: function (e) {
             console.log('error: ' + e.toString())
         }
-    })
+    });
+    lightGallery (document.getElementById('lightgallery'));
 });
