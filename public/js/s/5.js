4a6f4701eb89adb81114ece56f95b2f03c6d82e7
Zulcom <s01@ngs.ru>
2018-02-24 16:20:08 +0700

    Lab5

--- public/js/s/5.js
+++ public/js/s/5.js
@@ -4,15 +4,24 @@ $(function () {
         url:'/ajax/5',
         dataType: "json",
         success: function (d, e) {
-       console.log(d);
            $('#gallery').text(d.gallery);
            $.each(d.images, function (index, item) {
                $('#img-'+index+' img').attr('src',item.file).attr('alt',item.name).attr('width',400).attr('height',400);
+               $('#img-'+index).attr('href',item.file).attr('title',item.name);
            });
         },
         error: function (e) {
             console.log('error: ' + e.toString())
         }
     });
-    lightGallery (document.getElementById('lightgallery'));
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        mainClass: 'mfp-fade',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1]
+        }
+    });
 });
